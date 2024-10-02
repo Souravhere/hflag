198528da4bc64e007f501dc5538c713dad6c3ebd
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-02 19:16:25 +0530

    empty commit for vercel production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
+        
         {children}
       </body>
     </html>
