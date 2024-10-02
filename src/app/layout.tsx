75f8a363ca3c5933fd6c0636c1d0a2aeea8ab06b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-02 19:18:37 +0530

    header comp removed to check the production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "@/components/Header";
 
 
 
@@ -31,8 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
-        
         {children}
       </body>
     </html>
