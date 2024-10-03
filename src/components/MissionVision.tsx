9398277ac796edd188a9caa92ea2ddb90971b8fc
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-03 20:09:30 +0530

    few bugs are fixed for resolve the production error

--- src/components/MissionVision.tsx
+++ src/components/MissionVision.tsx
@@ -70,7 +70,7 @@ export default function MissionVision() {
             variants={itemVariants}
             className="lg:w-1/2 flex flex-wrap justify-center lg:justify-end gap-4"
           >
-            {values.map((value, index) => (
+            {values.map((value, _index) => (
               <motion.div
                 key={value.name}
                 variants={itemVariants}
